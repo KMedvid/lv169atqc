12645c11003e4edfbee662e47b8d58b99ea16952
softservedata <wapgprs@list.ru>
2015-12-25 19:33:08 +0200

    Add Logs

--- src/test/java/com/softserve/edu/LoginAdmin.java
+++ src/test/java/com/softserve/edu/LoginAdmin.java
@@ -5,44 +5,65 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class LoginAdmin {
+    public static final Logger logger = LoggerFactory.getLogger(LoginAdmin.class);
     private WebDriver driver;
     
     @BeforeClass
     public void oneTimeSetUp() {
+        logger.info("@BeforeClass - oneTimeSetUp() START");
         System.out.println("@BeforeClass - oneTimeSetUp() START");
         //driver = new FirefoxDriver();
         driver = new HtmlUnitDriver(true);
+        ((HtmlUnitDriver) driver).setJavascriptEnabled(true);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("@BeforeClass - oneTimeSetUp() DONE");
+        logger.info("@BeforeClass - oneTimeSetUp() DONE");
     }
 
     @AfterClass
     public void oneTimeTearDown() {
+        logger.info("oneTimeTearDown() - oneTimeSetUp() START");
         driver.quit();
         System.out.println("@AfterClass - oneTimeTearDown()");
+        logger.info("oneTimeTearDown() - oneTimeSetUp() DONE");
     }
 
     @Test
     public void testUnit() throws Exception {
+        logger.info("@Test -  testUnit() START");
         System.out.println("@Test -  testUnit() START");
+        logger.info("Goto http://localhost:8080/OMS");
         driver.get("http://localhost:8080/OMS");
+        logger.info("Creating Web Elements");
         driver.findElement(By.name("j_username")).sendKeys("iva");
         driver.findElement(By.name("j_password")).sendKeys("qwerty");
         driver.findElement(By.name("submit")).click();
+        logger.info("Goto Admin Info Page");
+        //
+        //  Thread.sleep(2000);
+        //File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // For example copy somewhere
+        //FileUtils.copyFile(scrFile, new File("test-output/screenshot_HtmlUnitDriver.png"));
+        //
+        logger.info("Goto Login Page");
         driver.findElement(By.xpath("//a[@class='spec']")).click();
+        logger.info("Logout Succesfull");
         System.out.println("@Test -  testUnit() DONE");
+        logger.info("@Test -  testUnit() DONE");
     }
     
-    @Test
+    //@Test
     public void testAdm() throws Exception {
+        logger.info("@Test -  testAdm() START");
 //        WebDriver driver = new FirefoxDriver();
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         // Precondition
@@ -67,10 +88,12 @@ public class LoginAdmin {
         //driver.quit();
         // Checking
         Assert.assertEquals(lastname, "horoshko");
+        logger.info("@Test -  testAdm() DONE");
     }
 
-    @Test
+    //@Test
     public void testInvalidLogin() throws InterruptedException {
+        logger.info("@Test -  testInvalidLogin() START");
 //        WebDriver driver = new FirefoxDriver();
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         // Precondition
@@ -94,6 +117,7 @@ public class LoginAdmin {
         //driver.quit();
         // Checking
         Assert.assertTrue(lastname.startsWith("Your login attempt was not successful, try again"));
+        logger.info("@Test -  testInvalidLogin() DONE");
     }
 
 }
