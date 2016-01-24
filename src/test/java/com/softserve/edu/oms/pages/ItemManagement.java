a67fabff9f58f034f45c5aa1c5ac0a98cf884f2d
OlehBB <oleh.blahiy@gmail.com>
2016-01-24 18:17:02 +0200

    Add ItemManagement page

---
+++ src/test/java/com/softserve/edu/oms/pages/ItemManagement.java
@@ -0,0 +1,18 @@
+package com.softserve.edu.oms.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ItemManagement {
+    private WebDriver driver;
+    //
+
+    public ItemManagement(WebDriver driver) {
+        this.driver = driver;
+        //
+    }
+
+    // Get Elements
+    // Set Data
+    // Business Logic
+
+}
